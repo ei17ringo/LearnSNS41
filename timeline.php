419575e724f51d99c1e6f7028dd888b7a91ebd58
ei17ringo <ei17ringo@gmail.com>
2018-05-29 09:50:23 +0800

    いいね済みのみの表示機能を作成

--- timeline.php
+++ timeline.php
@@ -130,7 +130,21 @@
           $record["like_flag"] = 0;
         }
 
-        $feeds[] = $record;
+        //いいね済みのみのリンクが押されたときは、配列に既にいいね！してるものだけを代入する
+
+        if (isset($_GET["feed_select"]) && ($_GET["feed_select"]== "likes") && ($record["like_flag"] == 1)){
+          $feeds[] = $record;
+        }
+
+        // feed_selectが指定されてないときは全件表示
+        if (!isset($_GET["feed_select"])){
+            $feeds[] = $record;
+        }
+
+        // 新着順が押されたとき、全件表示
+        if (isset($_GET["feed_select"]) && ($_GET["feed_select"]== "news") ){
+          $feeds[] = $record;
+        }
         // $arr[] = 'ほげ';
     }
 
@@ -214,8 +228,22 @@
     <div class="row">
       <div class="col-xs-3">
         <ul class="nav nav-pills nav-stacked">
-          <li class="active"><a href="timeline.php?feed_select=news">新着順</a></li>
-          <li><a href="timeline.php?feed_select=likes">いいね！済み</a></li>
+        <?php if (isset($_GET["feed_select"]) && ($_GET["feed_select"]=="likes")){ ?>
+          <li>
+            <a href="timeline.php?feed_select=news">新着順</a>
+          </li>
+          <li class="active">
+            <a href="timeline.php?feed_select=likes">いいね！済み</a>
+          </li>
+        <?php }else{ ?>
+          <li class="active">
+            <a href="timeline.php?feed_select=news">新着順</a>
+          </li>
+          <li>
+            <a href="timeline.php?feed_select=likes">いいね！済み</a>
+          </li>
+        <?php } ?>
+        
           <!-- <li><a href="timeline.php?feed_select=follows">フォロー</a></li> -->
         </ul>
       </div>
