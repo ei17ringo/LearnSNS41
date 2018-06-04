73a01311b32447509b66ba4c8671abe8a7e37918
ei17ringo <ei17ringo@gmail.com>
2018-06-04 11:16:29 +0800

    コメント数の反映

--- comment.php
+++ comment.php
@@ -20,6 +20,16 @@ $data = array($comment,$login_user_id,$feed_id);
 $stmt = $dbh->prepare($sql);
 $stmt->execute($data);
 
+// feedsテーブルにcommentのカウントをUpdateする
+// SQL文を作成
+$update_sql = "UPDATE `feeds` SET `comment_count` = `comment_count`+1 WHERE `id`=?";
+
+$update_data = array($feed_id);
+
+// SQL文実行
+$update_stmt = $dbh->prepare($update_sql);
+$update_stmt->execute($update_data);
+
 // timeline.php(一覧)にもどる
 header("Location: timeline.php");
 
